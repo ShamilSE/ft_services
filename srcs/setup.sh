ba942c72a07066745c48a8908f881253d007102f
mismene <ishamil86@gmail.com>
2021-03-06 12:41:22 +0300

    trying to open access in cluster to nginx

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -2,10 +2,9 @@
 
 minikube delete
 kubectl delete -f nginx/nginx.yaml
-minikube delete
+kubectl delete -f nginx/matallb/matallb.yaml
 
 minikube start --vm-driver=virtualbox
-
 minikube addons enable metallb
 
 eval $(minikube docker-env)
@@ -13,4 +12,5 @@ eval $(minikube docker-env)
 docker build -t nginx-image nginx
 
 kubectl apply -f nginx/nginx.yaml
+kubectl apply -f nginx/matallb/matallb.yaml
 
