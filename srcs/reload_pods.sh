2259dc6594691834c6685647acffa7f6b70f5ebd
mismene <ishamil86@gmail.com>
2021-03-07 11:44:07 +0300

    opened ports 80 and 443, metallb gived ip to nginx

--- srcs/reload_pods.sh
+++ srcs/reload_pods.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+kubectl delete -f nginx/nginx.yaml
 kubectl delete -f nginx/matallb/matallb.yaml
 
+eval $(minikube docker-env)
 docker build -t nginx-image nginx
 
 kubectl apply -f nginx/nginx.yaml
