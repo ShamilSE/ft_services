7fe9da0e610306423d338c10f65e000c79d1bf92
mismene <ishamil86@gmail.com>
2021-03-21 11:17:17 +0300

    wp raised

--- srcs/reload_pods.sh
+++ srcs/reload_pods.sh
@@ -3,13 +3,16 @@
 kubectl delete -f nginx/nginx.yaml
 kubectl delete -f nginx/matallb/matallb.yaml
 kubectl delete -f mysql/mysql.yaml
+kubeclt delete -f wordpress/wordpress.yaml
 
 #rm ~/.ssh/known_hosts
 
 eval $(minikube docker-env)
 docker build -t nginx-image nginx
 docker build -t mysql-image mysql
+docker build -t wordpress-image wordpress
 
 kubectl apply -f nginx/nginx.yaml
 kubectl apply -f nginx/matallb/matallb.yaml
 kubectl apply -f mysql/mysql.yaml
+kubectl apply -f wordpress/wordpress.yaml
--- srcs/setup.sh
+++ srcs/setup.sh
@@ -6,6 +6,7 @@ minikube delete
 kubectl delete -f nginx/nginx.yaml
 kubectl delete -f nginx/matallb/matallb.yaml
 kubectl delete -f mysql/mysql.yaml
+kubectl delete -f wordpress/wordpress.yaml
 
 #rm ~/.ssh/known_hosts
 
@@ -15,7 +16,9 @@ minikube addons enable metallb
 eval $(minikube docker-env)
 docker build -t nginx-image nginx
 docker build -t mysql-image mysql
+docler build -t wordpress-image wordpress
 
 kubectl apply -f nginx/nginx.yaml
 kubectl apply -f nginx/matallb/matallb.yaml
 kubectl apply -f mysql/mysql.yaml
+kubeclt apply -f wordpress/wordpress.yaml
